15c58dc17aed4e809a895aab7464b634eaab7a0b
Mahanthesh <monty.officio@gmail.com>
2019-03-04 18:27:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('Listening to port 4000');
+});
+
+//Static files 
+app.use(express.static('public'));
+
+//socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data);
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data)
+    });
+
+});
+
